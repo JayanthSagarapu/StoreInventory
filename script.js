b21cd9cbffe95994d73afa2f4d982c3729a699d2
JayanthSagarapu <sjayanthkumar1@gmail.com>
2023-04-25 12:51:48 +0530

    Inventory

---
+++ script.js
@@ -0,0 +1,113 @@
+const form = document.getElementById('form');
+const itemname = document.getElementById('item');
+const description = document.getElementById('description');
+const price = document.getElementById('price');
+const quantity = document.getElementById('quantity');
+const addbtn = document.getElementById('addbtn');
+const list = document.getElementById('list')
+const listItem = document.getElementById('list-item');
+
+const obj={
+    item : itemname.value,
+    description : description.value,
+    price : price.value,
+    quantity : quantity.value,
+}
+
+addbtn.addEventListener('click',postItemtoCrud);
+
+function postItemtoCrud(e){
+     e.preventDefault();
+
+    const obj={
+        item : itemname.value,
+        description : description.value,
+        price : price.value,
+        quantity : quantity.value,
+    }
+    
+    // showOnScreen(obj)
+    
+    axios.post("https://crudcrud.com/api/1c5f91e05d944a738407fc318717e889/inventoryData",obj)
+    .then((response) => {
+        showOnScreen(response.data)
+        console.log(response)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+}
+
+function showOnScreen(inputObject){
+
+    const li = document.createElement('li');
+    li.className = "list-group-item align-self-center w-100 mb-1 p-1 d-block"
+    li.id = "list-item"
+    
+    
+    const buy3btn = document.createElement('button');
+
+    buy3btn.className = "btn btn-sm float-right border-dark mr-3"
+    buy3btn.textContent = "Buy3";
+    
+    buy3btn.onclick = () =>{
+        inputObject.quantity = inputObject.quantity-3; 
+    
+        LiAppend()
+    }
+
+    const buy2btn = document.createElement('button');
+
+    buy2btn.className = "btn btn-sm float-right border-dark mr-3"
+    buy2btn.textContent = "Buy2";
+    
+
+    buy2btn.onclick = (e) =>{
+        inputObject.quantity = inputObject.quantity-2;
+        LiAppend()
+    }
+
+
+    const buy1btn = document.createElement('button');
+
+    buy1btn.className = "btn btn-sm float-right border-dark mr-3"
+    buy1btn.textContent = "Buy1";
+    
+
+    buy1btn.onclick = () =>{
+        inputObject.quantity = inputObject.quantity-1;
+        LiAppend()
+    
+    }
+
+    
+    function LiAppend(){
+        li.textContent = inputObject.item +"  "+ inputObject.description +"  "+ inputObject.price +"  "+ inputObject.quantity
+        li.append(buy3btn)
+        li.append(buy2btn)
+        li.append(buy1btn)
+        list.append(li)
+    }
+    
+    LiAppend()
+
+    form.reset()
+
+
+}
+
+window.addEventListener('DOMContentLoaded',reloadpage);
+        function reloadpage(){
+          axios
+          .get("https://crudcrud.com/api/1c5f91e05d944a738407fc318717e889/inventoryData")
+          .then((response) => {
+            for(var i=0; i< response.data.length;i++){
+              showOnScreen(response.data[i])
+            }
+            console.log(response);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+
+        }
